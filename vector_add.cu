54e857e247dbf8b3e9fb74572ef3e851f8c11756
peihan.xph <peihan.xph@antfin.com>
2020-12-01 15:26:42 +0800

    cuda

---
+++ vector_add.cu
@@ -0,0 +1,80 @@
+// includes, system
+#include <stdio.h>
+#include <iostream>
+
+// includes CUDA Runtime
+#include <cuda_runtime.h>
+
+using namespace std;
+
+// 两个向量加法kernel，grid和block均为一维
+__global__ void add(float* x, float * y, float* z, int n)
+{
+    // 获取全局索引
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    // 步长stride是整个grid的线程数,有时候向量的元素数很多，这时候可以将在每个线程实现多个元素（元素总数/线程总数）的加法，相当于使用了多个grid来处理
+    // 不过下面的例子一个线程只处理一个元素，所以kernel里面的循环是不执行的
+    int stride = blockDim.x * gridDim.x;
+    // if (threadIdx.x == 0) 
+    //     printf("Hello thread %d, stride=%f\n", threadIdx.x, stride) ;
+    for (int i = index; i < n; i += stride)
+    {
+        z[i] = x[i] + y[i];
+    }
+}
+
+int main()
+{
+    // 向量长度
+    int N = 1 << 20;
+    int nBytes = N * sizeof(float);
+    // 申请host内存
+    float *x, *y, *z;
+    x = (float*)malloc(nBytes);
+    y = (float*)malloc(nBytes);
+    z = (float*)malloc(nBytes);
+
+    // 初始化数据
+    for (int i = 0; i < N; ++i)
+    {
+        x[i] = 10.0;
+        y[i] = 20.0;
+    }
+
+    // 申请device内存
+    float *d_x, *d_y, *d_z;
+    cudaMalloc((void**)&d_x, nBytes);
+    cudaMalloc((void**)&d_y, nBytes);
+    cudaMalloc((void**)&d_z, nBytes);
+
+    // 将host数据拷贝到device
+    cudaMemcpy((void*)d_x, (void*)x, nBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy((void*)d_y, (void*)y, nBytes, cudaMemcpyHostToDevice);
+    // 定义kernel的执行配置
+    dim3 blockSize(256);
+    dim3 gridSize((N + blockSize.x - 1) / blockSize.x);
+    // 执行kernel
+    add << < gridSize, blockSize >> >(d_x, d_y, d_z, N);
+
+    // 将device得到的结果拷贝到host
+    cudaMemcpy((void*)z, (void*)d_z, nBytes, cudaMemcpyDeviceToHost);
+
+    // 检查执行结果
+    float maxError = 0.0;
+    for (int i = 0; i < N; i++){
+        maxError = fmax(maxError, fabs(z[i] - 30.0));
+        std::cout<<"z["<<i<<"]="<<z[i]<<std::endl;
+    }
+    std::cout << "最大误差: " << maxError << std::endl;
+
+    // 释放device内存
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_z);
+    // 释放host内存
+    free(x);
+    free(y);
+    free(z);
+
+    return 0;
+}
